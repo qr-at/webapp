33fa4585cd65a13e651437aad4e602fcb8fb0ddd
Joshua Ferris <jajjferris@gmail.com>
2021-12-26 19:16:02 -0500

    :sparkles: log in view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
-  import HelloWorld from "./components/HelloWorld.vue";
+  import Login from "./views/Login.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <login />
 </template>
-
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const login = async () => {
+    console.log("Logging you in!");
+  };
+</script>
+
+<template>
+  <main class="min-h-screen w-screen bg-gray-100 py-32 px-4 text-center">
+    <h1 class="text-5xl font-bold text-blue-700 mb-2">QR <span class="text-purple-400">@</span></h1>
+    <p class="text-gray-700 text-lg mb-6">Attendance tracking for classes taught by Joshua Ferris!</p>
+    <button @click="login" class="rounded-lg text-sm font-semibold px-4 py-2 border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-red-100 duration-200">
+      Log in with Google
+    </button>
+  </main>
+</template>
